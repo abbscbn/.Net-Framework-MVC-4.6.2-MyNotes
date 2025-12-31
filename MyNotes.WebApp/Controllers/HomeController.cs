d06299dcbf53d7f6d8ba741c3246d958ce831ce9
abbscbn <abbscbn@gmail.com>
2025-12-31 16:21:29 +0300

    Test için kontrol katmanına yorum eklendi

--- MyNotes.WebApp/Controllers/HomeController.cs
+++ MyNotes.WebApp/Controllers/HomeController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MyNotes.WebApp.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        // Yorum alanı
         public ActionResult Index()
         {
             return View();
